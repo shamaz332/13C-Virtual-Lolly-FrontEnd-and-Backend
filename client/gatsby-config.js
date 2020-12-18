d27ebb28922a559fe12f34d4a69c6a6250ec9479
shamaz332 <shamazsaeed332@gmail.com>
2020-12-18 21:17:55 +0500

    config updated

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,10 +1,19 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "Lolly",
+        // This is the field under which it's accessible
+        fieldName: "lolly",
+        // URL to query from
+        url: "https://bsthvhlkd5ey5jqjnwwxoqrb44.appsync-api.us-east-1.amazonaws.com/graphql",
+        headers: {
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          Authorization: "da2-57kpsil6ureetk5tvrohs22ge4",
+        },
+      },
+    },
+  ],
 }
