3ff1e4901cdc182514c28d1e9daf571de8bf7ab9
shamaz332 <shamazsaeed332@gmail.com>
2020-12-18 20:52:39 +0500

    pipiline code added

--- bck/lib/bck-stack.ts
+++ bck/lib/bck-stack.ts
@@ -2,193 +2,185 @@ import * as cdk from "@aws-cdk/core";
 import * as appsync from "@aws-cdk/aws-appsync";
 import * as ddb from "@aws-cdk/aws-dynamodb";
 import * as lambda from "@aws-cdk/aws-lambda";
-import * as CodePipeline from '@aws-cdk/aws-codepipeline'
-import * as CodePipelineAction from '@aws-cdk/aws-codepipeline-actions'
-import * as CodeBuild from '@aws-cdk/aws-codebuild'
-import { PolicyStatement } from '@aws-cdk/aws-iam';
-import * as s3 from '@aws-cdk/aws-s3'
-import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
-import * as cloudfront from '@aws-cdk/aws-cloudfront';
-import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as CodePipeline from "@aws-cdk/aws-codepipeline";
+import * as CodePipelineAction from "@aws-cdk/aws-codepipeline-actions";
+import * as CodeBuild from "@aws-cdk/aws-codebuild";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 
 export class BckStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-        // Creates the AppSync API
-        const api = new appsync.GraphqlApi(this, 'Api', {
-          name: "lolly",
-          schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
-          authorizationConfig: {
-            defaultAuthorization: {
-              authorizationType: appsync.AuthorizationType.API_KEY,
-              
-            },
-          },
-          xrayEnabled: true,
-        });
-            // Prints out the AppSync GraphQL endpoint to the terminal
-    new cdk.CfnOutput(this, "GraphQLAPIURL", {
-      value: api.graphqlUrl
-     });
- 
-     // Prints out the AppSync GraphQL API key to the terminal
-     new cdk.CfnOutput(this, "GraphQLAPIKey", {
-       value: api.apiKey || ''
-     });
- 
-     // Prints out the stack region to the terminal
-     new cdk.CfnOutput(this, "Stack Region", {
-       value: this.region
-     });
-
-     const lollyLambda = new lambda.Function(this, 'lollylambda', {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      handler: 'main.handler',
-      code: lambda.Code.fromAsset('functions'),
-      memorySize: 1024
+    // Creates the AppSync API
+    const api = new appsync.GraphqlApi(this, "Api", {
+      name: "lolly",
+      schema: appsync.Schema.fromAsset("graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+        },
+      },
+      xrayEnabled: true,
     });
-    
+    // Prints out the AppSync GraphQL endpoint to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIURL", {
+      value: api.graphqlUrl,
+    });
+
+    // Prints out the AppSync GraphQL API key to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    });
+
+    // Prints out the stack region to the terminal
+    new cdk.CfnOutput(this, "Stack Region", {
+      value: this.region,
+    });
+
+    const lollyLambda = new lambda.Function(this, "lollylambda", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "main.handler",
+      code: lambda.Code.fromAsset("functions"),
+      memorySize: 1024,
+    });
+
     // Set the new Lambda function as a data source for the AppSync API
-    const lambdaDs = api.addLambdaDataSource('lambdaDatasourcelolly', lollyLambda);
+    const lambdaDs = api.addLambdaDataSource(
+      "lambdaDatasourcelolly",
+      lollyLambda
+    );
 
-
-    
     lambdaDs.createResolver({
       typeName: "Query",
-      fieldName: "listVlolly"
+      fieldName: "listVlolly",
     });
-    
+
     lambdaDs.createResolver({
       typeName: "Mutation",
-      fieldName: "createVlolly"
+      fieldName: "createVlolly",
     });
-    
-    const lollyTable = new ddb.Table(this, 'CDKlolly', {
+
+    const lollyTable = new ddb.Table(this, "CDKlolly", {
       billingMode: ddb.BillingMode.PAY_PER_REQUEST,
       partitionKey: {
-        name: 'id',
+        name: "id",
         type: ddb.AttributeType.STRING,
       },
     });
     // enable the Lambda function to access the DynamoDB table (using IAM)
-    lollyTable.grantFullAccess(lollyLambda)
-    
-    // Create an environment variable that we will use in the function code
-    lollyLambda.addEnvironment('LOLLY_TABLE', lollyTable.tableName);
+    lollyTable.grantFullAccess(lollyLambda);
 
-// CI CD pipeline code 
+    // Create an environment variable that we will use in the function code
+    lollyLambda.addEnvironment("LOLLY_TABLE", lollyTable.tableName);
+
+    // CI CD pipeline code
     //Deploy Gatsby on s3 bucket
     const myBucket = new s3.Bucket(this, "lollybucket", {
-      versioned: true,       
-      websiteIndexDocument: "index.html"
+      versioned: true,
+      websiteIndexDocument: "index.html",
     });
-    const dist = new cloudfront.Distribution(this, 'myDistribution', {
-      defaultBehavior: { origin: new origins.S3Origin(myBucket) }
+    const dist = new cloudfront.Distribution(this, "myDistribution", {
+      defaultBehavior: { origin: new origins.S3Origin(myBucket) },
     });
 
     new s3Deployment.BucketDeployment(this, "deployStaticWebsite", {
-      sources: [s3Deployment.Source.asset("../../client/public")],
+      sources: [s3Deployment.Source.asset("../client/public")],
       destinationBucket: myBucket,
-      distribution: dist
+      distribution: dist,
     });
     new cdk.CfnOutput(this, "CloudFrontURL", {
-      value: dist.domainName
+      value: dist.domainName,
     });
 
-        // Artifact from source stage
-        const sourceOutput = new CodePipeline.Artifact();
+    // Artifact from source stage
+    const sourceOutput = new CodePipeline.Artifact();
 
-         // Artifact from build stage
+    // Artifact from build stage
     const S3Output = new CodePipeline.Artifact();
 
-
     //Code build action, Here you will define a complete build
-    const s3Build = new CodeBuild.PipelineProject(this, 's3Build', {
+    const s3Build = new CodeBuild.PipelineProject(this, "s3Build", {
       buildSpec: CodeBuild.BuildSpec.fromObject({
-        version: '0.2',
+        version: "0.2",
         phases: {
           install: {
             "runtime-versions": {
-              "nodejs": 12
+              nodejs: 12,
             },
             commands: [
-              'cd 13C-Virtual-Lolly-FrontEnd-and-Backend',
-              'cd client',
-              'npm i -g gatsby',
-              'npm install',
+              
+              "cd client",
+              "npm i -g gatsby",
+              "npm install",
             ],
           },
           build: {
-            commands: [
-              'gatsby build',
-            ],
+            commands: ["gatsby build"],
           },
         },
         artifacts: {
-          'base-directory': './13C-Virtual-Lolly-FrontEnd-and-Backend/client/public',   ///outputting our generated Gatsby Build files to the public directory
-          "files": [
-            '**/*'
-          ]
+          "base-directory":
+            "./client/public", ///outputting our generated Gatsby Build files to the public directory
+          files: ["**/*"],
         },
       }),
       environment: {
-        buildImage: CodeBuild.LinuxBuildImage.STANDARD_3_0,   ///BuildImage version 3 because we are using nodejs environment 12
+        buildImage: CodeBuild.LinuxBuildImage.STANDARD_3_0, ///BuildImage version 3 because we are using nodejs environment 12
       },
     });
 
     const policy = new PolicyStatement();
-    policy.addActions('s3:*');
-    policy.addResources('*');
+    policy.addActions("s3:*");
+    policy.addResources("*");
     s3Build.addToRolePolicy(policy);
 
-        ///Define a pipeline
-        const pipeline = new CodePipeline.Pipeline(this, 'GatsbyPipeline', {
-          crossAccountKeys: false,  //Pipeline construct creates an AWS Key Management Service (AWS KMS) which cost $1/month. this will save your $1.
-          restartExecutionOnUpdate: true,  //Indicates whether to rerun the AWS CodePipeline pipeline after you update it.
-        });
+    ///Define a pipeline
+    const pipeline = new CodePipeline.Pipeline(this, "GatsbyPipeline", {
+      crossAccountKeys: false, //Pipeline construct creates an AWS Key Management Service (AWS KMS) which cost $1/month. this will save your $1.
+      restartExecutionOnUpdate: true, //Indicates whether to rerun the AWS CodePipeline pipeline after you update it.
+    });
 
     ///Adding stages to pipeline
 
     //First Stage Source
     pipeline.addStage({
-      stageName: 'Source',
+      stageName: "Source",
       actions: [
         new CodePipelineAction.GitHubSourceAction({
-          actionName: 'Checkout',
-          owner: 'shamaz332',
-          repo: "github-repo-name",
-          oauthToken: cdk.SecretValue.secretsManager('GITHUB_TOKEN'), ///create token on github and save it on aws secret manager
-          output: sourceOutput,                                       ///Output will save in the sourceOutput Artifact
-          branch: "master",                                           ///Branch of your repo
+          actionName: "Checkout",
+          owner: "shamaz332",
+          repo: "13C-Virtual-Lolly-FrontEnd-and-Backend",
+          oauthToken: cdk.SecretValue.secretsManager("GITHUB_TOKEN"), ///create token on github and save it on aws secret manager
+          output: sourceOutput, ///Output will save in the sourceOutput Artifact
+          branch: "master", ///Branch of your repo
         }),
       ],
-    })
-
+    });
     pipeline.addStage({
-      stageName: 'Build',
+      stageName: "Build",
       actions: [
         new CodePipelineAction.CodeBuildAction({
-          actionName: 's3Build',
+          actionName: "s3Build",
           project: s3Build,
           input: sourceOutput,
           outputs: [S3Output],
         }),
       ],
-    })
+    });
 
     pipeline.addStage({
-      stageName: 'Deploy',
+      stageName: "Deploy",
       actions: [
         new CodePipelineAction.S3DeployAction({
-          actionName: 's3Build',
+          actionName: "s3Build",
           input: S3Output,
           bucket: myBucket,
         }),
       ],
-    })
-
-
-
+    });
   }
 }
