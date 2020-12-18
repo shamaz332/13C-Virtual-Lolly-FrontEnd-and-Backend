d755ee40a3ab2c4ff3e246817baebf90593773b9
shamaz332 <shamazsaeed332@gmail.com>
2020-12-18 19:32:54 +0500

    pipiline

--- bck/README.md
+++ bck/README.md
@@ -1,14 +1,1 @@
-# Welcome to your CDK TypeScript project!
-
-This is a blank project for TypeScript development with CDK.
-
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-## Useful commands
-
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+npm i @aws-cdk/aws-codepipeline @aws-cdk/aws-codepipeline-actions @aws-cdk/aws-codebuild @aws-cdk/aws-iam @aws-cdk/aws-s3 @aws-cdk/aws-s3-deployment @aws-cdk/aws-cloudfront @aws-cdk/aws-cloudfront-origins
--- bck/lib/bck-stack.ts
+++ bck/lib/bck-stack.ts
@@ -2,6 +2,14 @@ import * as cdk from "@aws-cdk/core";
 import * as appsync from "@aws-cdk/aws-appsync";
 import * as ddb from "@aws-cdk/aws-dynamodb";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as CodePipeline from '@aws-cdk/aws-codepipeline'
+import * as CodePipelineAction from '@aws-cdk/aws-codepipeline-actions'
+import * as CodeBuild from '@aws-cdk/aws-codebuild'
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+import * as s3 from '@aws-cdk/aws-s3'
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
 
 export class BckStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -68,5 +76,119 @@ export class BckStack extends cdk.Stack {
     
     // Create an environment variable that we will use in the function code
     lollyLambda.addEnvironment('LOLLY_TABLE', lollyTable.tableName);
+
+// CI CD pipeline code 
+    //Deploy Gatsby on s3 bucket
+    const myBucket = new s3.Bucket(this, "lollybucket", {
+      versioned: true,       
+      websiteIndexDocument: "index.html"
+    });
+    const dist = new cloudfront.Distribution(this, 'myDistribution', {
+      defaultBehavior: { origin: new origins.S3Origin(myBucket) }
+    });
+
+    new s3Deployment.BucketDeployment(this, "deployStaticWebsite", {
+      sources: [s3Deployment.Source.asset("../../client/public")],
+      destinationBucket: myBucket,
+      distribution: dist
+    });
+    new cdk.CfnOutput(this, "CloudFrontURL", {
+      value: dist.domainName
+    });
+
+        // Artifact from source stage
+        const sourceOutput = new CodePipeline.Artifact();
+
+         // Artifact from build stage
+    const S3Output = new CodePipeline.Artifact();
+
+
+    //Code build action, Here you will define a complete build
+    const s3Build = new CodeBuild.PipelineProject(this, 's3Build', {
+      buildSpec: CodeBuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            "runtime-versions": {
+              "nodejs": 12
+            },
+            commands: [
+              'cd 13C-Virtual-Lolly-FrontEnd-and-Backend',
+              'cd client',
+              'npm i -g gatsby',
+              'npm install',
+            ],
+          },
+          build: {
+            commands: [
+              'gatsby build',
+            ],
+          },
+        },
+        artifacts: {
+          'base-directory': './13C-Virtual-Lolly-FrontEnd-and-Backend/client/public',   ///outputting our generated Gatsby Build files to the public directory
+          "files": [
+            '**/*'
+          ]
+        },
+      }),
+      environment: {
+        buildImage: CodeBuild.LinuxBuildImage.STANDARD_3_0,   ///BuildImage version 3 because we are using nodejs environment 12
+      },
+    });
+
+    const policy = new PolicyStatement();
+    policy.addActions('s3:*');
+    policy.addResources('*');
+    s3Build.addToRolePolicy(policy);
+
+        ///Define a pipeline
+        const pipeline = new CodePipeline.Pipeline(this, 'GatsbyPipeline', {
+          crossAccountKeys: false,  //Pipeline construct creates an AWS Key Management Service (AWS KMS) which cost $1/month. this will save your $1.
+          restartExecutionOnUpdate: true,  //Indicates whether to rerun the AWS CodePipeline pipeline after you update it.
+        });
+
+    ///Adding stages to pipeline
+
+    //First Stage Source
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new CodePipelineAction.GitHubSourceAction({
+          actionName: 'Checkout',
+          owner: 'shamaz332',
+          repo: "github-repo-name",
+          oauthToken: cdk.SecretValue.secretsManager('GITHUB_TOKEN'), ///create token on github and save it on aws secret manager
+          output: sourceOutput,                                       ///Output will save in the sourceOutput Artifact
+          branch: "master",                                           ///Branch of your repo
+        }),
+      ],
+    })
+
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new CodePipelineAction.CodeBuildAction({
+          actionName: 's3Build',
+          project: s3Build,
+          input: sourceOutput,
+          outputs: [S3Output],
+        }),
+      ],
+    })
+
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [
+        new CodePipelineAction.S3DeployAction({
+          actionName: 's3Build',
+          input: S3Output,
+          bucket: myBucket,
+        }),
+      ],
+    })
+
+
+
   }
 }
